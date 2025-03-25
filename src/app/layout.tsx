a7789e69df2314456fc13333ba5137b6a41fd627
rohitk131 <rohitkumar13103@gmail.com>
2025-03-25 10:06:44 +0530

    added og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { Poppins } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import "./globals.css";
+
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "600", "700"], 
-  style: ["normal", "italic"], 
-  display: "swap", 
+  weight: ["400", "600", "700"],
+  style: ["normal", "italic"],
+  display: "swap",
 });
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -21,7 +23,28 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "SnipSnap",
-  description: "A smart clipboard for all your links, specially for your ai chats.",
+  description: "A smart clipboard for all your links, especially for your AI chats.",
+  openGraph: {
+    title: "SnipSnap-Your Smart Clipboard for AI Chats & Links",
+    description: "Save, organize, and access your AI conversations and important links effortlessly.",
+    url: "https://snipsnap-extension.vercel.app/", 
+    siteName: "SnipSnap",
+    images: [
+      {
+        url: "/og-image.png", 
+        width: 1200,
+        height: 630,
+        alt: "SnipSnap-Smart Clipboard for AI Chats",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SnipSnap-Your Smart Clipboard for AI Chats & Links",
+    description: "Save, organize, and access your AI conversations and important links effortlessly.",
+    images: ["/og-image.png"], 
+  },
 };
 
 export default function RootLayout({
@@ -31,9 +54,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased ${poppins.className}`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased ${poppins.className}`}>
         {children}
         <Analytics />
       </body>
